73da2ade97f46d359deea57caab19b24a343954f
Sebastian Daschner <git@sebastian-daschner.de>
2018-02-21 06:49:04 -0800

    updated

--- src/main/java/com/sebastian_daschner/instrument_craft_shop/HealthResource.java
+++ src/main/java/com/sebastian_daschner/instrument_craft_shop/HealthResource.java
@@ -1,28 +1,14 @@
 package com.sebastian_daschner.instrument_craft_shop;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.ServiceUnavailableException;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @Path("health")
-@ApplicationScoped
 public class HealthResource {
-
-    private AtomicBoolean shutdown = new AtomicBoolean();
 
     @GET
     public String healthCheck() {
-        if (shutdown.get())
-            throw new ServiceUnavailableException();
-        return "ok";
-    }
-
-    @DELETE
-    public void shutdown() {
-        shutdown.set(false);
+        return "OK";
     }
 
 }
