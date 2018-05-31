1f95f5b22cc921bf47b6225bd7a0dae65e70dbb5
Sebastian Daschner <git@sebastian-daschner.de>
2018-05-31 14:00:33 +0200

    added resiliency concerns;

--- src/main/java/com/sebastian_daschner/instrument_craft_shop/boundary/InstrumentsResource.java
+++ src/main/java/com/sebastian_daschner/instrument_craft_shop/boundary/InstrumentsResource.java
@@ -1,7 +1,10 @@
 package com.sebastian_daschner.instrument_craft_shop.boundary;
 
+import com.airhacks.porcupine.execution.boundary.Dedicated;
 import com.sebastian_daschner.instrument_craft_shop.entity.Instrument;
 
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -9,6 +12,8 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 
 @Path("instruments")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -18,21 +23,27 @@ public class InstrumentsResource {
     @Inject
     InstrumentCraftShop instrumentCraftShop;
 
+    @Inject
+    @Dedicated("instruments-read")
+    ExecutorService readExecutor;
+
+    @Inject
+    @Dedicated("instruments-write")
+    ExecutorService writeExecutor;
+
     @GET
-    public List<Instrument> getInstruments() {
-        return instrumentCraftShop.getInstruments();
+    public CompletableFuture<List<Instrument>> getInstruments() {
+        return CompletableFuture.supplyAsync(() -> instrumentCraftShop.getInstruments(), readExecutor);
     }
 
     @POST
-    public Response createInstrument(@Valid @NotNull Instrument instrument) {
-        try {
-            instrumentCraftShop.craftInstrument(instrument);
-            return Response.noContent().build();
-        } catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .header("X-Error", e.getMessage())
-                    .build();
-        }
+    public CompletableFuture<Response> createInstrument(@Valid @NotNull Instrument instrument) {
+        return CompletableFuture
+                .runAsync(() -> instrumentCraftShop.craftInstrument(instrument), writeExecutor)
+                .thenApply(c -> Response.noContent().build())
+                .exceptionally(e -> Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                        .header("X-Error", e.getMessage())
+                        .build());
     }
 
 }
--- src/main/java/com/sebastian_daschner/instrument_craft_shop/control/MakerBot.java
+++ src/main/java/com/sebastian_daschner/instrument_craft_shop/control/MakerBot.java
@@ -1,6 +1,8 @@
 package com.sebastian_daschner.instrument_craft_shop.control;
 
 import com.sebastian_daschner.instrument_craft_shop.entity.InstrumentType;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -12,6 +14,7 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
+import java.util.concurrent.TimeUnit;
 
 @ApplicationScoped
 public class MakerBot {
@@ -21,10 +24,14 @@ public class MakerBot {
 
     @PostConstruct
     private void initClient() {
-        client = ClientBuilder.newBuilder().build();
+        client = ClientBuilder.newBuilder()
+                .connectTimeout(2, TimeUnit.SECONDS)
+                .readTimeout(4, TimeUnit.SECONDS)
+                .build();
         target = client.target("http://maker-bot:9080/maker-bot/resources/jobs");
     }
 
+    @CircuitBreaker
     public void printInstrument(InstrumentType type) {
         JsonObject requestBody = createRequestBody(type);
         Response response = sendRequest(requestBody);
